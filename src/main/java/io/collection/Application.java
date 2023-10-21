7d10e0b47eaf9e99481cee5a3f784601e7885b88
juliawatanabe <julia.awv@gmail.com>
2023-10-21 18:15:16 -0300

    implemented exception

--- src/main/java/io/collection/Application.java
+++ src/main/java/io/collection/Application.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"java.io.collection.model", "java.io.colletction.repository"})
+@ComponentScan({"java.io.collection.model", "java.io.collection.repository", "java.io.collection.service", "java.io.collection.controller"})
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
+	
